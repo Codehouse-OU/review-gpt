148ca2d3a7077f433db82056f69918bdc9f104a1
MarkusKarileet <markus@helmes.com>
2024-10-22 16:52:35 +0300

    code review improvements

--- reviewgpt/review/review_interface.py
+++ reviewgpt/review/review_interface.py
@@ -1,8 +1,8 @@
 class ReviewInterface:
-    def execute(self, diff: str) -> str:
+    def review(self, diff: str) -> str:
         """
         Review the diff content and generate a response. When there are no comments to be made, return "NO_COMMENTS".
-        When there are modifications, the system shoud respond with an array of comments:
+        When there are modifications, the system should respond with an array of comments:
         [
             {
                 "path": "path/to/file",
