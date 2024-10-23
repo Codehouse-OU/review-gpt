e29a3b05a803ef9ffa4620b7e01fc0c0c2db7e8d
MarkusKarileet <markus@helmes.com>
2024-10-23 12:24:14 +0300

    fix failing test

--- tests/test_app.py
+++ tests/test_app.py
@@ -23,7 +23,7 @@ def test_bot_webhook_invalid_request(client, monkeypatch):
     monkeypatch.setattr('reviewgpt.app_service.AppService.is_valid_request', mock_is_valid_request)
     rv = client.post('/', json={})
     json_data = rv.get_json()
-    assert rv.status_code == 400
+    assert rv.status_code == 401
     assert json_data['message'] == 'Invalid request'
 
 
